3c8d16b83e89636b59e46e9b3eeeffd20cba774a
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-01 05:06:09 +0300

    feat: support source capo in mono v1.1

--- src/writers/canonicalTabJsonWriter.js
+++ src/writers/canonicalTabJsonWriter.js
@@ -4,6 +4,7 @@ const { EngineError } = require('../errors/engineError');
 const {
   CanonicalTabContractError,
   validateCanonicalTabResult,
+  validateCanonicalTabResultV1_1,
 } = require('../contracts/canonicalTabResultContract');
 
 class CanonicalTabJsonWriterError extends EngineError {
@@ -104,6 +105,17 @@ function validateForJsonWriter(canonicalTabResult) {
   try {
     return validateCanonicalTabResult(canonicalTabResult);
   } catch (error) {
+    if (
+      error instanceof CanonicalTabContractError
+      && error.code === 'UNSUPPORTED_CANONICAL_TAB_SCHEMA'
+      && error.details.actual === '1.1.0'
+    ) {
+      try {
+        return validateCanonicalTabResultV1_1(canonicalTabResult);
+      } catch (v1_1Error) {
+        throw adaptContractError(v1_1Error);
+      }
+    }
     throw adaptContractError(error);
   }
 }
