7f7b0baa3d5555915d36f0845a106f20c33704e9
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-01 05:05:57 +0300

    feat: support source capo in mono v1.1

--- src/contracts/canonicalTabContractMetadata.js
+++ src/contracts/canonicalTabContractMetadata.js
@@ -2,8 +2,10 @@
 
 const ENGINE_NAME = 'musicxml-to-guitar-tab-engine';
 const CANONICAL_TAB_RESULT_VERSION = '1.0.0';
+const CANONICAL_TAB_RESULT_V1_1_VERSION = '1.1.0';
 
 module.exports = {
   ENGINE_NAME,
   CANONICAL_TAB_RESULT_VERSION,
+  CANONICAL_TAB_RESULT_V1_1_VERSION,
 };
