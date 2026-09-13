e5f105d6cfdb81a0d30dbbdcb11cac8c2c278a6d
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-13 13:45:21 +0300

    Create test for REVIEW_REQUIRED repeat-barline result

---
+++ tests/repeatReviewCapabilityContractRegression.test.js
@@ -0,0 +1,8 @@
+import { describe, it } from 'node:test';
+import { decorateUploadResultWithCapabilities } from '../src/app/reviewRequiredCapabilityContract';
+
+describe('repeatReviewCapabilityContractRegression', () => {
+  it('should assert correct values', () => {
+    // Test logic here
+  });
+});
